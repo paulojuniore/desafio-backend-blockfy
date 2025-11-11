f8ee5252adbdc2f805f6af3c383dabe2d500ea33
paulojuniore <paulojuniore98@gmail.com>
2025-11-11 17:14:48 -0300

    Set basic configs, inital migration and some fields added  in models.

--- communication/models.py
+++ communication/models.py
@@ -9,17 +9,11 @@ class PixMessage(models.Model):
   campoLivre = models.TextField(blank=True, null=True)
   txId = models.CharField(max_length=50)
   dataHoraPagamento = models.DateTimeField(auto_now_add=True)
-
-  def __str__(self):
-    return self.end_to_end_id
+  visualizado = models.BooleanField(default=False)
 
 class PixStream(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+  is_active = models.BooleanField(default=True)
   ispb = models.CharField(max_length=8)
-  interation_id = models.CharField(max_length=12, unique=True, default='')
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
-
-  def save(self, *args, **kwargs):
-    if not self.iteration_id:
-        self.iteration_id = uuid.uuid4().hex[:12]
-    super().save(*args, **kwargs)
