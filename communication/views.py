04f7470e549fec8f9a97e22a84b3821df6333f19
paulojuniore <paulojuniore98@gmail.com>
2025-11-13 09:48:15 -0300

    Refactoring functions to include long pooling behavior and generic function to return messages.

--- communication/views.py
+++ communication/views.py
@@ -6,18 +6,23 @@ from rest_framework.response import Response
 from .models import PixMessage, PixStream
 from rest_framework.views import APIView
 from datetime import datetime,  timezone
-from django.http import JsonResponse
 from rest_framework import status
 from faker import Faker
 import random
+import time
 import uuid
 
 # Classe para gerar mensagens Pix com dados falsos.
 fake = Faker('pt_BR')
 
+MAX_STREAMS_PER_ISPB = 6
+MAX_MESSAGES_PER_STREAM = 10
+LONG_POLLING_TIMEOUT = 8  # segundos
+
 @method_decorator(csrf_exempt, name='dispatch')
 class PixMessageView(APIView):
   
+  # Gera mensagens Pix falsas para um determinado ISPB.
   def post(self, request, ispb, number):
     try:
       number = int(number)
@@ -72,37 +77,56 @@ class FallbackJSONRenderer(JSONRenderer):
 class PixStreamStartView(APIView):
   renderer_classes = [FallbackJSONRenderer]
 
+  # Inicia uma nova sessão de stream para obter mensagens Pix.
   def get(self, request, ispb):
+    active_streams = PixStream.objects.filter(ispb=ispb, is_active=True)
+    if active_streams.count() >= MAX_STREAMS_PER_ISPB:
+      return Response({'error': 'Maximum number of active streams reached.'}, status=status.HTTP_429_TOO_MANY_REQUESTS)
+
     header = request.headers.get('Accept', 'application/json')
     multiple = header == 'multipart/json'
-    
-    # Busca todas as mensagens não visualizadas.
-    all_receiver_messages = PixMessage.objects.filter(recebedor__ispb=ispb, visualizado=False)
-  
-    # Restringe o número de mensagens (10 no máximo) retornadas com base no cabeçalho Accept enviado.
-    filtered_receiver_messages = list(all_receiver_messages[:10 if multiple else 1])
-
-    # Se não houver mensagens, retorna 204 No Content.
-    if not all_receiver_messages:
-      return Response(status=status.HTTP_204_NO_CONTENT)
-    
-    # Marca as mensagens como visualizadas para não serem mais retornadas.
-    all_receiver_messages \
-      .filter(id__in=[msg.id for msg in filtered_receiver_messages]) \
-      .update(visualizado=True)
     
     # Cria uma nova sessão de stream.
     stream_session = PixStream.objects.create(ispb=ispb)
 
-    # Forma o link Pull-Next para ser retornado.
-    pull_next = f"/api/pix/{ispb}/stream/{stream_session.id}"
+    response = self._get_messages(ispb, stream_session, multiple)
+    response["Pull-Next"] = f"/api/pix/{ispb}/stream/{stream_session.id}"
+    return response
+  
+  # Método auxiliar para obter mensagens com long polling.
+  def _get_messages(self, ispb, stream, multiple):
+    start_time = time.time()
+    while time.time() - start_time < LONG_POLLING_TIMEOUT:
 
-    return _find_session_and_get_response_api(multiple, filtered_receiver_messages, pull_next)
+      # Busca todas as mensagens não visualizadas.
+      all_receiver_messages = PixMessage.objects.filter(recebedor__ispb=ispb, visualizado=False)
+
+      # Restringe o número de mensagens (10 no máximo) retornadas com base no cabeçalho Accept enviado.
+      filtered_receiver_messages = list(all_receiver_messages[:10 if multiple else 1])
+
+      # Marca as mensagens como visualizadas para não serem mais retornadas.
+      if all_receiver_messages.exists():
+        all_receiver_messages \
+          .filter(id__in=[msg.id for msg in filtered_receiver_messages]) \
+          .update(visualizado=True)
+
+        # Serializa as mensagens e retorna a resposta.
+        data = PixMessageSerializer(filtered_receiver_messages, many=True).data   
+        return Response(data, status=status.HTTP_200_OK)
+      
+      # Aguarda um curto período antes de verificar novamente.
+      time.sleep(5)
+    
+    # Se o tempo limite for atingido sem novas mensagens, retorna 204 No Content.
+    response = Response(status=status.HTTP_204_NO_CONTENT)
+    response["Pull-Next"] = f"/api/pix/{ispb}/stream/{stream.id}"
+    return response
 
   
 class PixStreamNextView(APIView):
   renderer_classes = [FallbackJSONRenderer]
 
+  # Obtém as próximas mensagens em uma sessão de stream existente.
   def get(self, request, ispb, interationId):
     stream_session = PixStream.objects.filter(id=interationId, ispb=ispb).first()
     if not stream_session:
@@ -111,43 +135,13 @@ class PixStreamNextView(APIView):
     header = request.headers.get('Accept', 'application/json')
     multiple = header == 'multipart/json'
     
-    # Busca todas as mensagens não visualizadas.
-    all_receiver_messages = PixMessage.objects.filter(recebedor__ispb=ispb, visualizado=False)
-
-    # Se não houver mensagens, retorna 204 No Content.
-    if len(all_receiver_messages) == 0:
-      return Response(status=status.HTTP_204_NO_CONTENT)
-  
-    # Restringe o número de mensagens (10 no máximo) retornadas com base no cabeçalho Accept enviado.
-    filtered_receiver_messages = list(all_receiver_messages[:10 if multiple else 1])
-    
-    # Marca as mensagens como visualizadas para não serem mais retornadas.
-    all_receiver_messages \
-      .filter(id__in=[msg.id for msg in filtered_receiver_messages]) \
-      .update(visualizado=True)
-    
-    # Forma o link Pull-Next para ser retornado.
-    pull_next = f"/api/pix/{ispb}/stream/{stream_session.id}"
-
-    return _find_session_and_get_response_api(multiple, filtered_receiver_messages, pull_next)
-  
-  def delete(self, request, ispb, interationId):
-    stream_session = PixStream.objects.filter(id=interationId, ispb=ispb).first()
-    stream_session.delete()
-    return Response(data={}, status=status.HTTP_200_OK)
-
-# Função genérica para os retornos das chamadas HTTP GET e evitar repetição de código nas views de stream.
-def _find_session_and_get_response_api(multiple, filtered_receiver_messages, pull_next):
-  serializer = PixMessageSerializer(filtered_receiver_messages, many=True)
-  
-  # Retorna uma única mensagem com o cabeçalho Pull-Next.
-  if not multiple:
-    response = Response(serializer.data[0], content_type='application/json', status=status.HTTP_200_OK)
-    response["Pull-Next"] = pull_next
+    response = PixStreamStartView._get_messages(self, ispb, stream_session, multiple)
+    response["Pull-Next"] = f"/api/pix/{ispb}/stream/{stream_session.id}"
     return response
   
-  # Retorna múltiplas mensagens com o cabeçalho Pull-Next.
-  response = JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
-  response["Pull-Next"] = pull_next
-
-  return response
+  # O stream não é removido, apenas marcado como inativo.
+  def delete(self, request, ispb, interationId):
+    stream_session = PixStream.objects.filter(id=interationId, ispb=ispb).first()
+    stream_session.is_active = False
+    stream_session.save()
+    return Response(data={}, status=status.HTTP_200_OK)
